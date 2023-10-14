3131ba93a12e5c3081bb084382a755878c151944
vladimir <49815384+vladivaniuc@users.noreply.github.com>
2023-10-14 09:37:01 +0300

    added proxy url to solve CORS error

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
 // get quotes from API
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const quoteAuthor = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
 let apiQuotes = [];
 
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function hideLoadingSpinner() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 function newQuote() {
+    showLoadingSpinner();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log("new quote is", quote);
-    console.log("quote text is", quote.text);
-    console.log("quote authro is", quote.author);
-    return quote;
+    
+    if (!quote.author) {
+        quoteAuthor.textContent = "uknown";
+    } else {
+        quoteAuthor.textContent = quote.author;
+    }; 
+
+    if (quote.text.length>50) {
+        quoteText.classList.add("long-quote")
+    } else {
+        quoteText.classList.remove("long-quote");
+    }
+    quoteText.textContent = quote.text;
+    hideLoadingSpinner();
 }
 
 async function getQuotes() {
-    const apiUrl = 'https://type.fit/api/quotes';
+    showLoadingSpinner();
+    const proxyUrl = 'https://cors.bridged.cc/';
+    const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
-        const response = await fetch(apiUrl);
+        const response = await fetch(proxyUrl+apiUrl);
         apiQuotes = await response.json();
         return newQuote();
     }
@@ -22,20 +52,13 @@ async function getQuotes() {
         }
 };
 
+//Tweet Quote
+function tweetQuote() {
+    const twitterURL = `https://twitter.com/intent/tweet?text=${quoteText.text} - ${quoteAuthor.textContent}`;
+    window.open(twitterURL, '_blank');
+};
 
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
-async function insertNewQuote() {
-    let currentQuote = document.getElementById("quote");
-    let currentAuthor = document.getElementById("author");
-    try {
-        let newQuote = await getQuotes();
-        currentQuote.innerText = newQuote.text;
-        currentAuthor.innerText = newQuote.author;
-    }
-    catch (error) {
-        console.log(error);
-    }
-}
-
-let nextButton = document.getElementById("new-quote");
-nextButton.addEventListener("click", insertNewQuote);
+getQuotes();
